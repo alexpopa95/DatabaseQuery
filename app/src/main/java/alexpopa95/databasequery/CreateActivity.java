459ce712069b96437b0b10a813d12ef44db3274a
alexpopa95 <alexpopa95@gmail.com>
2015-03-17 10:02:18 +0100

    First commit

---
+++ app/src/main/java/alexpopa95/databasequery/CreateActivity.java
@@ -0,0 +1,16 @@
+package alexpopa95.databasequery;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by alexpopa95 on 17/03/15.
+ */
+public class CreateActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crea_tabella);
+    }
+}
---
+++ app/src/main/java/alexpopa95/databasequery/InsertActivity.java
@@ -0,0 +1,19 @@
+package alexpopa95.databasequery;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * Created by alexpopa95 on 17/03/15.
+ */
+public class InsertActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.inserisci_tabella);
+    }
+}
